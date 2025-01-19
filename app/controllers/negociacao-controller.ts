d5f2ae2f14fc7adcc4dfd3e01a6a3fa42ebf2757
Wellington2708 <wellingtondevelopercontact@gmail.com>
2025-01-19 18:42:30 +0000

    refatorando instancia

--- app/controllers/negociacao-controller.ts
+++ app/controllers/negociacao-controller.ts
@@ -10,7 +10,7 @@ export class NegociacaoController{
     private inputQuantidade : HTMLInputElement;
     private inputValor : HTMLInputElement;
     private negociacoes = new Negociacoes();
-    private negociacoesView = new NegociacoesView('#negociacoesView');
+    private negociacoesView = new NegociacoesView('#negociacoesView', true);
     private mensagemView = new MensagemView('#mensagemView');
 
     
@@ -23,7 +23,11 @@ export class NegociacaoController{
     }
 
     public adiciona() : void{
-       const negociacao = this.criaNegociacao();
+       const negociacao = Negociacao.criaDe(
+        this.inputData.value,
+        this.inputQuantidade.value,
+        this.inputValor.value
+       );
        if(!this.ehDiaUtil(negociacao.data)){
         this.mensagemView.update('Apenas negociações em dias úteis são aceitas');
         return ;
@@ -40,14 +44,7 @@ export class NegociacaoController{
          && data.getDay() < DiasDaSemana.SABADO
     }
 
-    private criaNegociacao() : Negociacao {
-         //expressão regular or expression regular
-        const exp = /-/g;
-        const date = new Date(this.inputData.value.replace(exp, ','));
-        const quantidade = parseInt(this.inputQuantidade.value);
-        const valor = parseFloat(this.inputValor.value);
-        return new Negociacao(date, quantidade, valor)
-    }
+ 
 
     private limparFormulario(){
         this.inputData.value = "";
--- app/models/negociacao.ts
+++ app/models/negociacao.ts
@@ -23,4 +23,13 @@ export class Negociacao {
         return this.quantidade * this._valor;
     }
 
+    public static criaDe(dataString : string, quantidadeString : string, valorString : string) : Negociacao{
+          //expressão regular or expression regular
+          const exp = /-/g;
+          const date = new Date(dataString.replace(exp, ','));
+          const quantidade = parseInt(quantidadeString);
+          const valor = parseFloat(valorString);
+          return new Negociacao(date, quantidade, valor)
+    }
+
 }
